a4d015feba9c2a23143e868bdfbe6a845745bd77
JungEunChong <21900673@handong.edu>
2020-06-13 21:43:05 +0900

    free - 3

--- wiid.c
+++ wiid.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 
 int fib(int n);
+int ffib(int n);
 
 int main() {
 	int a;
 	printf("a : ");
 	scanf("%d", &a);
+
+	printf("fib  : ");
 	
 	for(int i = 1; i <= a; i++) {	
 		printf("%d ", fib(i));
+	}
+
+	printf("\nffib : ");
+	
+	for(int i = 1; i <= a; i++) {
+		printf("%d ", ffib(i));
 	}
 
 	printf("\n");
@@ -20,4 +29,10 @@ int fib(int n) {
 	if(n == 1 || n == 2) return 1;
 
 	return fib(n-1) + fib(n-2);
+}
+
+int ffib(int n) {
+	if(n == 1 || n == 2 || n == 3) return 1;
+
+	return ffib(n-1) + fib(n-2);
 }
